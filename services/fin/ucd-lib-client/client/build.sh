1e17029022d67b645699302567c36425a1456f0b
Justin Merz <jrmerz@gmail.com>
2023-03-16 13:25:31 -0700

    fix for dams fcrepo docker build

--- services/fin/ucd-lib-client/client/build.sh
+++ services/fin/ucd-lib-client/client/build.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+set -e
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $ROOT_DIR
+
 rm -rf dist
 mkdir dist
 
-
 cp -r public/images dist/
+cp -r public/fonts dist/
 cp -R -L public/loader dist/
 
 cp public/index.html dist/
