29c28298dfaa9a549e5377d6eb54394581f2eb19
Justin Merz <jrmerz@gmail.com>
2023-03-20 09:29:12 -0700

    fix for initial dev image build

--- services/fin/ucd-lib-client/client/dist.sh
+++ services/fin/ucd-lib-client/client/dist.sh
@@ -4,8 +4,20 @@ set -e
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $ROOT_DIR
 
+rm -rf dist
+mkdir dist
+
+cp -r public/images dist/
+cp -r public/fonts dist/
+cp -R -L public/loader dist/
+
+cp public/index.html dist/
+cp public/jwt.html dist/
+cp public/ie.html dist/
+cp public/manifest.json dist/
+
 # Hack fix
 cp public/node_modules/@internetarchive/bookreader/BookReader/icons/fullscreen_exit.svg \
    public/node_modules/@internetarchive/bookreader/BookReader/icons/fullscreen-exit.svg
 
-webpack --config webpack.config.js
+webpack --config webpack-dist.config.js
