ec00840f07a86c9c13469886c42fa8fdd8c3cea9
Justin Merz <jrmerz@gmail.com>
2023-03-23 13:25:12 -0700

    adding itemCount to root node for collection #29

--- services/fin/models/collection/model.js
+++ services/fin/models/collection/model.js
@@ -6,6 +6,10 @@ class CollectionsModel extends FinEsDataModel {
 
   constructor() {
     super('collection');
+
+    // we lookup item counts for a collection by querying the item index
+    this.itemAlias = 'item-read';
+
     this.schema = schema;
     this.transformService = 'es-collection-transform';
   }
@@ -15,15 +19,65 @@ class CollectionsModel extends FinEsDataModel {
     return false;
   }
 
-  // get(id, opts={}) {
-  //   return super.get(id, opts);
-  // }
+  /**
+   * @method get
+   * @description override get to add itemCount to collection
+   * 
+   * @param {*} id 
+   * @param {*} opts 
+   * @param {*} index 
+   * @returns 
+   */
+  async get(id, opts={}, index) {
+    let result = await super.get(id, opts={}, index);
+    if( result ) await this.getItemCount(result);
+    return result;
+  }
 
-  // getItemCount(collection) {
-  //   let ids = []; 
-  //   let node = utils.singleNode(id, result['@graph'])
-  //   collection['@graph'].map(item => item['@id']);
-  // }
+  /**
+   * @method search
+   * @description override search to add itemCount to collection
+   * 
+   * @param {*} searchDocument 
+   * @param {*} options 
+   * @param {*} index 
+   */
+  async search(searchDocument, options={debug:false}, index) {
+    let response = await super.search(searchDocument, options, index);
+    for( let result of response.results ) {
+      await this.getItemCount(result);
+      console.log(result);
+    }
+    return response;
+  }
+
+  /**
+   * @method getItemCount
+   * @description set the itemCount property on a collections root node
+   * 
+   * @param {Object} collection collection graph
+   * 
+   * @returns {Promise}
+   */
+  async getItemCount(collection) {
+    let result = await this.client.count({
+      index : this.itemAlias,
+      body: {
+        query: {
+          bool : {
+            must : [
+              {term: {'@graph.isPartOf.@id': collection['@id']}}
+            ]
+          }
+        }
+      }
+    });
+
+    let graph = this.utils.singleNode(collection['@id'], collection['@graph']);
+    if( graph.length ) {
+      graph[0].itemCount = result.count;
+    }
+  }
 }
 
 module.exports = new CollectionsModel();
