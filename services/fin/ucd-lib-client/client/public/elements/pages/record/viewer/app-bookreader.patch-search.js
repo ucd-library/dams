c96e926c58373f5acf220fd16490526608b4d3c3
UcDust <dcartwright@ucdavis.edu>
2024-02-28 09:57:04 -0800

    override page tops for mobile / 1page mode

--- services/fin/ucd-lib-client/client/public/elements/pages/record/viewer/app-bookreader.patch-search.js
+++ services/fin/ucd-lib-client/client/public/elements/pages/record/viewer/app-bookreader.patch-search.js
@@ -1,5 +1,6 @@
 
 const {poll} = require('@internetarchive/bookreader/src/BookReader/utils.js');
+const { DragScrollable } = require('@internetarchive/bookreader/src/BookReader/DragScrollable.js');
 
 module.exports = function patchSearch(BookReader) {
   /**
@@ -71,6 +72,166 @@ module.exports = function patchSearch(BookReader) {
       $boxes.removeAttr("style");
     }
   };
+
+  BookReader.prototype.switchMode = function (
+    mode,
+    {
+      suppressFragmentChange = false,
+      init = false,
+      pageFound = false
+    } = {}
+  ) {
+    // Skip checks before init() complete
+    if (this.init.initComplete) {
+      if (mode === this.mode) {
+        return;
+      }
+      if (!this.canSwitchToMode(mode)) {
+        return;
+      }
+    }
+  
+    this.trigger(BookReader.eventNames.stop);
+  
+    this.prevReadMode = this.getPrevReadMode(this.mode);
+  
+    if (this.mode != mode) {
+      this.activeMode.unprepare?.();
+    }
+  
+    this.mode = mode;
+  
+    // reinstate scale if moving from thumbnail view
+    if (this.pageScale !== this.reduce) {
+      this.reduce = this.pageScale;
+    }
+  
+    // $$$ TODO preserve center of view when switching between mode
+    //     See https://bugs.edge.launchpad.net/gnubook/+bug/416682
+  
+    // XXX maybe better to preserve zoom in each mode
+    if (this.constMode1up == mode) {
+      _prepareMode1Up(this._modes.mode1Up);
+    } else if (this.constModeThumb == mode) {
+      this.reduce = this.quantizeReduce(this.reduce, this.reductionFactors);
+      this._modes.modeThumb.prepare();
+    } else {
+      this._modes.mode2Up.prepare();
+    }
+  
+    if (!(this.suppressFragmentChange || suppressFragmentChange)) {
+      this.trigger(BookReader.eventNames.fragmentChange);
+    }
+    const eventName = mode + 'PageViewSelected';
+    this.trigger(BookReader.eventNames[eventName]);
+  
+    this.textSelectionPlugin?.stopPageFlip(this.refs.$brContainer);
+  };
+
+  function _prepareMode1Up(mode) {
+    const startLeaf = mode.br.currentIndex();
+    mode.$brContainer
+      .empty()
+      .css({ overflow: 'hidden' })
+      .append(mode.$el);
+
+    // override so that page size can be max possible width/height
+    mode.mode1UpLit.renderPage = function(page) {
+      return _mode1UpRenderPage(page, mode.mode1UpLit);
+    };
+
+    mode.mode1UpLit.computePageTops = _computePageTops;
+
+    // Need this in a setTimeout so that it happens after the browser has _actually_
+    // appended the element to the DOM
+    setTimeout(async () => {
+      if (!mode.everShown) {
+
+        mode.mode1UpLit.initFirstRender(startLeaf);
+        mode.everShown = true;
+        mode.mode1UpLit.requestUpdate();
+        await mode.mode1UpLit.updateComplete;
+        new DragScrollable(mode.mode1UpLit, {
+          preventDefault: true,
+          dragSelector: '.br-mode-1up__visible-world',
+          // Only handle mouse events; let browser/interact.js handle touch
+          dragstart: 'mousedown',
+          dragcontinue: 'mousemove',
+          dragend: 'mouseup',
+        });
+      }
+      mode.mode1UpLit.jumpToIndex(startLeaf);
+      setTimeout(() => {
+        // Must explicitly call updateVisibleRegion, since no
+        // scroll event seems to fire.
+        mode.mode1UpLit.updateVisibleRegion();
+      });
+    });
+    mode.br.updateBrClasses();
+  }
+
+  function _computePageTops(pages, spacing) {
+    // calc height of images in inches
+    // get max height and width of images
+    let minHeight = Math.min(...pages.map(page => page.height));
+    let maxWidth = Math.max(...pages.map(page => page.width));
+    let height = this.offsetWidth / maxWidth * minHeight - 40;
+
+    // hack for mobile resolutions
+    let widthOver500 = window.innerWidth - 500;
+    if( widthOver500 < 0 ) widthOver500 = 0;
+    let pageHeightInches = (pages[0]?.ppi || 500) / (height - widthOver500);
+
+    const result = {};
+    let top = spacing;
+    for (const page of pages) {
+      result[page.index] = top;
+      top += pageHeightInches + spacing;
+    }
+    return result;
+  }
+
+  function _mode1UpRenderPage(page, litElement) {
+    // adjust width/height to be max possible width/height possible in container
+    const wToR = litElement.coordSpace.worldUnitsToRenderedPixels;
+    const wToV = litElement.coordSpace.worldUnitsToVisiblePixels;
+    const containerWidth = litElement.coordSpace.visiblePixelsToWorldUnits(litElement.htmlDimensionsCacher.clientWidth);
+
+    // image width/height in px
+    const width = wToR(page.widthInches);
+    const height = wToR(page.heightInches);
+    
+    // container width/height in px
+    const widthOffset = litElement.offsetWidth - 15;
+    const heightOffset = litElement.offsetHeight - 15;
+ 
+    // calc max width/height possible
+    let widthRatio = widthOffset / width;
+    let heightRatio = heightOffset / height;
+    let scaleRatio = Math.min(widthRatio, heightRatio);
+    
+    const left = Math.max(litElement.SPACING_IN, (containerWidth - (page.widthInches * scaleRatio)) / 2);
+
+    // pageTops updated above in _computePageTops()
+    const top = litElement.pageTops[page.index];
+
+    const transform = `translate(${wToR(left)}px, ${wToR(top)}px)`;
+    
+    const pageContainerEl = litElement.createPageContainer(page)
+      .update({
+        dimensions: {
+          width: width * scaleRatio,
+          height: height * scaleRatio,
+          top: 0,
+          left: 0,
+        },
+        reduce: page.width / wToV(page.widthInches),
+      }).$container[0];
+
+    pageContainerEl.style.transform = transform;
+    pageContainerEl.classList.toggle('BRpage-visible', litElement.visiblePages.includes(page));
+    return pageContainerEl;
+  }
 
   class UcdBookReader extends BookReader {
     /**
