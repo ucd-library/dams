3d792bbfaa1490e1d55a5c63691a613965a2073e
Dusty Cartwright <103536473+UcDust@users.noreply.github.com>
2025-07-01 14:42:11 -0700

    add lastModified schema index to collection sort, hide ui-admin image tilt ui (#357)
    * add lastModified to collection sort #354
    
    * revert ui sorting into seperate release version, to build collection index first
    
    * hide image tilt admin ui functionality #359
    
    * fixes from code review

--- services/fin/ucd-lib-client/client/public/lib/models/CollectionModel.js
+++ services/fin/ucd-lib-client/client/public/lib/models/CollectionModel.js
@@ -110,9 +110,10 @@ class CollectionModel extends BaseModel {
       // TODO: change to 'uploadDate'
       let searchDocument = {
         limit,
-        sort: [{
-          '@graph.yearPublished': {order : "desc" } 
-        }]
+        sort: [
+          {'@graph.yearPublished': {order : "desc" }},
+          // {'@graph.lastModified': {order : "desc" }} // this will be used in a subsequent release
+        ]
       };
       // searchDocument = {limit: 3};
       return this.search(searchDocument);
