1932ca0261d84cdeef8ed0ce003c37a576366f66
Dusty Cartwright <dcartwright@ucdavis.edu>
2025-12-12 09:19:31 -0800

    fix for bookreader off-by-1 for selected page downloads #414

--- services/fin/ucd-lib-client/client/public/elements/pages/record/app-media-download.js
+++ services/fin/ucd-lib-client/client/public/elements/pages/record/app-media-download.js
@@ -196,9 +196,12 @@ export default class AppMediaDownload extends Mixin(LitElement).with(
       // 2 page image download
       this.isTwoPageView = true;      
 
+      // if switching from single to two page view on odd page, selected page is the second page in the displayed bookreader view
+      let startIndex = pageNumber % 2 !== 0 ? pageNumber - 2 : pageNumber - 1;
+
       // set download href to 2 pages for archive download option
-      let image1 = pages[pageNumber - 1]?.download?.url?.replace('/fcrepo/rest', '');
-      let image2 = pages[pageNumber]?.download?.url?.replace('/fcrepo/rest', '');
+      let image1 = pages[startIndex]?.download?.url?.replace('/fcrepo/rest', '');
+      let image2 = pages[startIndex + 1]?.download?.url?.replace('/fcrepo/rest', '');
       let urls = [];
       if( image1 ) urls.push(image1);
       if( image2 ) urls.push(image2);
@@ -457,13 +460,14 @@ export default class AppMediaDownload extends Mixin(LitElement).with(
       this.showDownloadLabel = true;
     }
     
+    this._setDownloadHref(this.sources);
+
     if( this.isBookreader && !this.fullSetSelected ) {
       this._onBookreaderStateUpdate(this.BookReaderModel.getState());
     } else {
       urls = sources.map(s => s.url.replace('/fcrepo/rest', ''));
     }
     
-    this._setDownloadHref(this.sources);
     this._setZipPaths(urls);
   }
 
