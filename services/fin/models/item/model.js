d5b6ab04ddedccd968a81080e31939717d182fd6
Dusty Cartwright <dcartwright@ucdavis.edu>
2025-12-15 15:01:45 -0800

    revert console.log

--- services/fin/models/item/model.js
+++ services/fin/models/item/model.js
@@ -28,9 +28,7 @@ class ItemsModel extends FinEsDataModel {
         '_score',
         { 'name.raw' : 'asc' }
       ]
-    }
-    console.log('ItemsModel search', JSON.stringify(searchDocument, null, 2));
-    
+    }    
     return super.search(searchDocument, options, index);
   }
 
