9971bcd5532760ce4f9c09472de7225d6a2ea404
Dusty Cartwright <dcartwright@ucdavis.edu>
2026-05-07 11:10:07 -0700

    form field missing a label #433

--- services/fin/ucd-lib-client/client/public/elements/pages/record/viewer/app-media-viewer.tpl.js
+++ services/fin/ucd-lib-client/client/public/elements/pages/record/viewer/app-media-viewer.tpl.js
@@ -259,6 +259,7 @@ export default function render() {
                 type="text"
                 id="br-search-input"
                 autocomplete="off"
+                aria-label="Search inside this item"
                 @change="${this._onBRSearch}"
               />
 
